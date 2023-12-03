12388c4b9a8e65c7fbafb1b4c6fbda918452a1a7
vldngls <vldngls.0418@gmail.com>
2023-12-03 12:29:31 +0800

    Base Commit

--- app/src/main/java/com/example/afd_summativeexam/MainMenu.java
+++ app/src/main/java/com/example/afd_summativeexam/MainMenu.java
@@ -2,7 +2,7 @@ package com.example.afd_summativeexam;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainMenu extends AppCompatActivity {
@@ -13,22 +13,16 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         // Slam book Button
-        findViewById(R.id.slambookMain).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Intent to start SlamBookMain activity
-                Intent intent = new Intent(MainMenu.this, SlamBookMain.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.slambookMain).setOnClickListener(view -> {
+            // Intent to start SlamBookMain activity
+            Intent intent = new Intent(MainMenu.this, SlamBookMain.class);
+            startActivity(intent);
         });
 
 
         // Logout TextView
-        findViewById(R.id.logoutTextView).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();  // Assuming you want to go back to the previous activity
-            }
+        findViewById(R.id.logoutTextView).setOnClickListener(view -> {
+            finish();  // Assuming you want to go back to the previous activity
         });
     }
 }
